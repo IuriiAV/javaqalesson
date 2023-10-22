6ae3a57946ad7906c7f5275947a0cf56d8697315
IuriiAV <iurii.vl.avramenko@gmail.com>
2023-10-22 13:40:56 +0300

    add homework one, add slides for 1,2 lessons

---
+++ src/main/java/com/qatelran/org/lessontwo/homework/Product.java
@@ -0,0 +1,5 @@
+package com.qatelran.org.lessontwo.homework;
+
+public class Product {
+    // Класс содержит переменную с наименованием продукта, методами для доступа к ней
+}
---
+++ src/main/java/com/qatelran/org/lessontwo/homework/Storage.java
@@ -0,0 +1,4 @@
+package com.qatelran.org.lessontwo.homework;
+
+public class Storage {
+}
