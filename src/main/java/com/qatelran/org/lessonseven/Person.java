70db29acce8e617ed298c64c54cc056bc99462bc
IuriiAv <Yurasha22r22@>
2023-11-24 12:39:35 +0300

    lesson seven (set), part one

---
+++ src/main/java/com/qatelran/org/lessonseven/Person.java
@@ -0,0 +1,9 @@
+package com.qatelran.org.lessonseven;
+
+public class Person implements Comparable<Person> {
+
+    @Override
+    public int compareTo(Person o) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/qatelran/org/lessonseven/TestSetPractice.java
@@ -0,0 +1,27 @@
+package com.qatelran.org.lessonseven;
+
+import java.util.*;
+
+public class TestSetPractice {
+
+    public static void main(String[] args) {
+        List<String> stringList = Arrays.asList("A", "B", "E", "D", "E", "B");
+        Set<String> stringSet = new TreeSet<>();
+        for (String str : stringList) {
+            stringSet.add(str);
+        }
+        System.out.println(stringSet);
+
+        //создать список из элементов, которые встречаются два и более раза
+        List<String> doubles = new ArrayList<>();
+        Set<String> unique = new TreeSet<>();
+        for (String str : stringList) {
+            if (unique.contains(str)) {
+                doubles.add(str);
+            } else {
+                unique.add(str);
+            }
+        }
+        System.out.println(doubles);
+    }
+}
